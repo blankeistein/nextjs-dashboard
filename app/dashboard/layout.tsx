c6a80acb59a5728b2d876d4289b132fe0a08a775
blankeistein <blankeistein@gmail.com>
2024-03-07 16:03:20 +0700

    fix:type parameter layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SideNav from "@/app/ui/dashboard/sidenav";
 
-export default function Layout({ children }: { children: React.ReactDOM }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
